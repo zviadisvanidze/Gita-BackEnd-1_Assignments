7b21fbef3ea4a687738ed1cab1fa54352713a9b9
Zviadi Svanidze <zviadisva@gmail.com>
2026-07-10 12:11:57 +0400

    .

--- Lec18/src/index.ts
+++ Lec18/src/index.ts
@@ -94,24 +94,6 @@ console.log(`Max Number: ${maxNumber}`);
 console.log(`Is Palindrome: ${isPalindromeResult}`);
 console.log(`Factorial: ${factorialResult}`);
 
-/*
-
-2. შევქმნათ კლასი BankAccount რომელსაც ექნება accountNumber, balance და transactionHistory ფროფერთები.
-   კონსტრუქტორში უნდა ვიღებდეთ accountNumber და initialBalance მნიშვნელობებს.
-   გარედან არუნდა იყოს შესაძლებელი accountNumber, balance და transactionHistory შეცვლა.
-   კლასში უნდა გვქონდეს მეთოდები:
-   getAccountInfo
-   deposit - თანხის დამატება ანგარიშზე.
-   withdraw - თანხის მოკლება ანგარიშიდან.
-   transferFunds - გადარიცხვა სხვა BankAccount_ზე
-   getTransactionHistory - აბრუნებს transactionHistory_ მასივს
-   recordTransaction - transactionHistory_ში ამატებს ჩნაწერს ტრანსფერის შესახებ
-
-   შევქმნათ მინიმუმ 2 BankAccount_ის ინსტანსი.
-   გავაკეთოთ სხვადასხვა ოპერაციები.
-   დავბეჯდოთ შექმნილი ექაუნთების transactionHistory.
-
-*/
 
 interface Transaction {
   type: string;
